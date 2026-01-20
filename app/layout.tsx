a9ea2ec7fbe2b2a1f07405f5136a944edd602154
Flynnzito_Nic <Lzfts593@gmail.com>
2026-01-20 12:18:50 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/favicon-32x32.png',
         type: 'image/svg+xml',
       },
     ],
